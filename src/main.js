53473b90c92c2a8f82f12ee612ce42e8960272f9
Manasvi Priyadarshi <manasvi.p@byjus.com>
2021-06-29 23:12:03 +0530

    removed logs

--- src/main.js
+++ src/main.js
@@ -26,9 +26,6 @@ var firebaseConfig = {
   "measurementId": import.meta.env.VITE_MEASUREMENT_ID
 }
 
-console.log(process.env);
-console.log(import.meta.env);
-
 firebase.initializeApp(firebaseConfig);
 
 library.add(faChevronUp, faChevronDown);
