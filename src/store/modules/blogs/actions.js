c80b4c9753af72b50b591779a518a523fda7a113
Manasvi Priyadarshi <manasvi2001@gmail.com>
2021-06-30 16:51:53 +0000

    Merged in development (pull request #7)
    Development
    
    * Merged in feature/googleAuth (pull request #1)
    
    Fixed google login and added header
    
    * Fixed google login and added header
    
    * Merged in feature/createBlog (pull request #2)
    
    Feature/createBlog
    
    * Added readme
    
    * Added readme
    
    * Added the functionality for creating a blog
    
    * Removed logging
    
    * Merge branch 'feature/createBlog' of bitbucket-bivcccswb:bivcccswb/thetoxify-client into feature/createBlog
    
    * Merged in feature/UIEnhancements (pull request #4)
    
    testing changes
    
    * testing changes
    
    * Merge branch 'master' of bitbucket-bivcccswb:bivcccswb/thetoxify-client into development
    
    * resolve conflict
    
    * Merge branch 'development' of bitbucket-bivcccswb:bivcccswb/thetoxify-client into development
    
    * Merged in feature/blog (pull request #6)
    
    Feature/blog
    
    * fixing stuff 3
    
    * fixing stuff 4
    
    * fixing stuff 5
    
    * fixing stuff 6
    
    * removed logs
    
    * check 1
    
    * check 2
    
    * Merged in feature/googleAuth (pull request #1)
    
    Fixed google login and added header
    
    * Fixed google login and added header
    
    * Merged in feature/createBlog (pull request #2)
    
    Feature/createBlog
    
    * Added readme
    
    * Added readme
    
    * Added the functionality for creating a blog
    
    * Removed logging
    
    * Merge branch 'feature/createBlog' of bitbucket-bivcccswb:bivcccswb/thetoxify-client into feature/createBlog
    
    * Added blogid

--- src/store/modules/blogs/actions.js
+++ src/store/modules/blogs/actions.js
@@ -5,10 +5,11 @@ export default {
 
     }
     let payload = await response.json();
-    context.commit('fetchBlogs', payload);
+    let blogs = Object.values(payload);
+    context.commit('fetchBlogs', blogs);
   },
   async createBlog(context, payload) {
-    let response = await fetch("https://thetoxify-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/blogs.json", {
+    let response = await fetch(`https://thetoxify-firebase-default-rtdb.asia-southeast1.firebasedatabase.app/blogs/${payload.id}.json`, {
       method: 'PUT',
       body: JSON.stringify(payload)
     });
---
+++ src/util/utility.js
@@ -0,0 +1,11 @@
+export default {
+  getUniqueBlogId() {
+    return "ttblg-" + this.getId();
+  },
+  getId() {
+    // Math.random should be unique because of its seeding algorithm.
+    // Convert it to base 36 (numbers + letters), and grab the first 9 characters
+    // after the decimal.
+    return Math.random().toString(36).substr(2, 9);
+  }
+}
